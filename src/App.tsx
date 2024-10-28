0236fdae769ad3b3631d757097e06ee2aeb7d568
aw-sxt <alex.williams@spaceandtime.io>
2024-10-28 15:02:01 -0500

    refactor: dir structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Footer from './components/Footer'
+
 
 function App() {
   return (
     <>
-      <Header/>
-      <Hero/>
-      <Footer/>
     </>
   )
 }
